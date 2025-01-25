98379418ea34a491994a347f31f644e35ba1953b
ABIHAAHMED4262 <abihaahmed413@gmail.com>
2025-01-25 18:37:41 +0500

    update styling

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <section className="about" style={{backgroundImage:"url(/images/about-bg.webp)"}}>
+      <section className="about" style={{ backgroundImage: "url(/images/about-bg.webp)" }}>
         <div className="about-content">
           <h1 className="fade-in">About Us</h1>
           <p>
@@ -11,11 +11,11 @@ const About = () => {
             craftsmanship meets elegance. At <b>Watch World</b>, we are
             passionate about creating watches that combine precision, style, and
             durability. Each piece is thoughtfully designed to enhance your
-            personality, whether you're seeking a classic design or a modern
+            personality, whether you&apos;re seeking a classic design or a modern
             statement. With a commitment to quality and attention to detail, we
             aim to deliver timepieces that not only tell time but also tell a
             story. Explore our collection and find the perfect watch to
-            accompany you on every occasion
+            accompany you on every occasion.
           </p>
         </div>
       </section>
